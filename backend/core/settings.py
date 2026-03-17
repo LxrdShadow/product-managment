5f62a44b507284709a8b08c20baee0b19b882d0c
LxrdShadow <andritiana.idealy@gmail.com>
2026-03-17 20:42:50 +0300

    feat(settings): add app settings like title and description to settings

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -4,6 +4,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    TITLE: str = "Product management"
+    DESCRIPTION: str = "School project for product management"
+    VERSION: str = "1.0.0"
+
     DATABASE_URL: str
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
