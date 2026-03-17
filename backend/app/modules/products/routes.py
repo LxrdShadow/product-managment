5691d25dcc73de2cd7bb9fb1e6cfa0eb2f783f9b
LxrdShadow <andritiana.idealy@gmail.com>
2026-03-17 20:52:35 +0300

    fix(products): fix dependency error in routes

--- backend/app/modules/products/routes.py
+++ backend/app/modules/products/routes.py
@@ -1,14 +1,17 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status
 
 from app.modules.products.dependencies import get_product_service
 from app.modules.products.schema import ProductCreate, ProductOut
+from app.modules.products.service import ProductService
 
 router = APIRouter(tags=["Products"], prefix="/products")
 
 
 @router.post("/", response_model=ProductOut)
-async def create_product(product: ProductCreate, service: get_product_service):
+async def create_product(
+    product: ProductCreate, service: ProductService = Depends(get_product_service)
+):
     try:
-        return service.create_product(product)
+        return await service.create_product(product)
     except Exception as e:
         raise HTTPException(status.HTTP_400_BAD_REQUEST, {"error": str(e)})
