84e6e1581714b76f449c2518fc9c6771e2cd3df0
LxrdShadow <andritiana.idealy@gmail.com>
2026-03-17 22:15:13 +0300

    feat(products): create update function in the repository

--- backend/app/modules/products/repository.py
+++ backend/app/modules/products/repository.py
@@ -1,4 +1,6 @@
-from sqlalchemy import delete, insert, select
+from typing import Optional
+
+from sqlalchemy import delete, insert, select, update
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.modules.products.models import Product
@@ -29,5 +31,16 @@ class ProductRepository:
     async def delete(self, number: str) -> Product:
         """Delete a product from the database"""
         stmt = delete(Product).where(Product.number == number).returning(Product)
+        result = await self.session.execute(stmt)
+        return result.scalar_one_or_none()
+
+    async def update(self, number: str, **data) -> Optional[Product]:
+        """Update a product according to the data given"""
+        stmt = (
+            update(Product)
+            .where(Product.number == number)
+            .values(**data)
+            .returning(Product)
+        )
         result = await self.session.execute(stmt)
         return result.scalar_one_or_none()
