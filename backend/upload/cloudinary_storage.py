a008b3994bf626be4c689d500685fadc33a62677
LxrdShadow <andritiana.idealy@gmail.com>
2026-03-22 16:12:19 +0300

    feat(upload): introduce cloudinary storage

---
+++ backend/upload/cloudinary_storage.py
@@ -0,0 +1,19 @@
+import cloudinary
+import cloudinary.uploader
+
+from core.settings import get_settings
+from upload.storage import Storage
+
+settings = get_settings()
+cloudinary.config(
+    cloud_name=settings.CLOUDINARY_CLOUD_NAME,
+    api_key=settings.CLOUDINARY_API_KEY,
+    api_secret=settings.CLOUDINARY_API_SECRET,
+    secure=True,
+)
+
+
+class CloudinaryStorage(Storage):
+    def save(self, path: str, content: bytes) -> str:
+        result = cloudinary.uploader.upload(content, public_id=path)
+        return result.get("public_id")
